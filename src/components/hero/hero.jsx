a886f5cf28f8cc3bf292922c7299f41326b0ea2c
Dinith Pamoda <dinithpgurusinghe830@gmail.com>
2024-03-17 17:31:52 +0530

    css item

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import './hero.css'
-import hand_icon from '../assets/hand_icon.png'
-import arrow_icon from '../assets/arrow_icon.png'
 import hero_image from '../assets/hero.png'
+import arrow_icon from '../assets/arrow_icon.png'
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
-        <div>
-          <div className="hand-hand-icon">
-            <p>new</p>
-            <img src={hand_icon} alt=""/>
-          </div>
-          <p>collection</p>
-          <p>for everyone</p>
+        <h2>FRESH FLOWERS ONLY</h2>
+        <div className='hero-text'>
+          <p>Fresh Flower Collection</p>
         </div>
         <div className="hero-latest-btn">
-          <div>Latest Collection</div>
+          <div>Fresh Roses</div>
           <img src={arrow_icon} alt=""/>
         </div>
       </div>
